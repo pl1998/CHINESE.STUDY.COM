e8d456b0d6de92769069621ae9f85c653a061f9b
pl1998 <pltruenine@163.com>
2025-05-12 11:39:56 +0800

    update

---
+++ app/Enum/CacheEnum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enum;
+
+class CacheEnum
+{
+    /**
+     *  网站配置
+     * @var string
+     */
+    const CONFIG_SITE = 'cache:config:site';
+    /**
+     *  邮箱配置
+     * @var string
+     */
+    const CONFIG_EMAIL = 'cache:config:email';
+    /**
+     *  支付配置
+     * @var string
+     */
+    const CONFIG_PAY = 'cache:config:pay';
+}   
