85cfe1c94c9346d7c21206ef913506440ca9a616
pl1998 <2139714249#qq.com>
2025-05-18 10:45:03 +0800

    feat:定时任务

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\MailConfigServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
     ])->toArray(),
 
     /*
