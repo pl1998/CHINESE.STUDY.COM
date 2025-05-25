7993dce888804147503e63b4dd96e4562a43da90
pl1998 <2139714249#qq.com>
2025-05-25 11:18:33 +0800

    update

--- app/Console/Commands/MailSendCommand.php
+++ app/Console/Commands/MailSendCommand.php
@@ -31,33 +31,6 @@ class MailSendCommand extends Command
      */
     public function handle()
     {
-
- 
-$response = Http::withHeaders([
-    'Accept' => 'application/json',
-    'Accept-Language' => 'en_US',
-])->withBasicAuth(
-    'Af3jciHfbwVqAdst04P3EC7rzdIWTSzPSAZOdPLILOVuPn5hx79qGhzjnaaYo_9RgTgN7pV-En7lMW-6',  // 你的 PayPal Client ID
-    'EHhMRsdk4lzYHGyLFhYX49MZ3CVzwZXZsDddRfeVJelctSlCVdycYV4AsPtwfD_tNba11HhMjSiTqkEm'  // 你的 PayPal Secret
-)->asForm()->post('https://api.paypal.com/v1/oauth2/token', [
-    'grant_type' => 'client_credentials'
-]);
-
-// 获取响应数据
-$data = $response->json();
-dd($data);
-// 检查是否成功
-if ($response->successful()) {
-    // 成功获取 token
-    $accessToken = $data['access_token'];
-    // 使用 token...
-} else {
-    // 处理错误
-    $error = $data['error'] ?? 'Unknown error';
-    $errorDescription = $data['error_description'] ?? 'No description';
-    // 记录或处理错误...
-}
-
         $emailConfig = ConfigEmail::getConfig();
         if(!empty($emailConfig)){
          config([
