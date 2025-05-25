12d6f6e4dbeafad7afd41eca11b448dacd9e3846
pl1998 <2139714249#qq.com>
2025-05-25 11:35:43 +0800

    feat:支付配置优化

--- config/paypal.php
+++ config/paypal.php
@@ -9,7 +9,7 @@ return [
     'sandbox' => [
         'client_id'         => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
         'client_secret'     => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
-        'app_id'            => 'APP-80W284485P519543T',
+        'app_id'            => env('PAYPAL_SANDBOX_APP_ID', ''),
     ],
     'live' => [
         'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
@@ -23,6 +23,7 @@ return [
     'locale'         => env('PAYPAL_LOCALE', 'en_US'), // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
     'validate_ssl'   => env('PAYPAL_VALIDATE_SSL', true), // Validate SSL when creating api client.
     'http' => [
-        'verify' => false,
+        'verify' => env('PAYPAL_HTTP_VERIFY', true),
     ],
 ];
+
